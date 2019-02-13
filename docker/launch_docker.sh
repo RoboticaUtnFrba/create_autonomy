391dce7e1c9b4aacfd8db0b44c16092758eccb76
Emiliano Borghi <emiliano_borghi@hotmail.com>
2019-02-12 23:36:05 -0300

    Docker container: first commit

---
+++ docker/launch_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+xhost +
+
+DOCKER_CAPABILITIES="--ipc=host \
+                     --cap-add=IPC_LOCK \
+                     --cap-add=sys_nice"
+
+DOCKER_NETWORK="--network=host"
+
+docker run -it \
+--privileged --rm \
+${DOCKER_CAPABILITIES} \
+${DOCKER_NETWORK} \
+--env="DISPLAY"  \
+--env="QT_X11_NO_MITSHM=1"  \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--workdir="/home/$USER/catkin_ws" \
+--volume="/home/$USER/catkin_ws:/home/$USER/catkin_ws" \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+--volume="$HOME/host_docker:/home/user/host_docker" \
+-e LOCAL_USER_ID=`id -u $USER` \
+-e LOCAL_GROUP_ID=`id -g $USER` \
+-e LOCAL_GROUP_NAME=`id -gn $USER` \
+ create-melodic-gazebo9
