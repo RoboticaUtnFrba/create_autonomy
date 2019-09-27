3e9c3d736355e53dd29c0169796cbf32b58ea3b4
Lucas J. Scheinkerman <lucas.scheinkerman@gmail.com>
2019-09-27 10:27:25 -0300

    Changed draw_square launch file node name and type

--- ca_tools/scripts/draw_square.py
+++ ca_tools/scripts/draw_square.py
@@ -14,13 +14,13 @@ class DrawSquare():
     _linear = None
     _angular = None
 
-    def __init__(self,first_pt,second_pt,third_pt)
+    def __init__(self,first_pt,second_pt,third_pt):
         self._interface = interface
         self._pub_cmd = rospy.Publisher('cmd_vel', Twist)
 
         self._hz = rospy.get_param('~hz', 10)
 
-    def _publish()
+    def _publish():
         twist = Twist()
         twist.linear.x = 0.7
         twist.linear.y = 0.0
@@ -30,7 +30,7 @@ class DrawSquare():
         twist.angular.z = 0.0
         self._pub_cmd.publish(twist)
 
-    def run(self)
+    def run(self):
         self._linear = 0
         self._angular = 0
 
