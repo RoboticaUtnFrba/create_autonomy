c07bb0229012ac07ed2182277c8949f9ddd662aa
Lucas J. Scheinkerman <lucas.scheinkerman@gmail.com>
2019-10-29 14:43:48 -0300

    Changed client and server python files, launchfile and line follower msg file. Package working.

--- ca_tools/scripts/line_follower_client.py
+++ ca_tools/scripts/line_follower_client.py
@@ -1,17 +1,15 @@
 #! /usr/bin/env python
 
-
 import roslib
 roslib.load_manifest('ca_tools')
 import rospy
 import actionlib
 
-from ca_actions.msg import LineFollowerAction, LineFollowerGoal
+from ca_actions.msg import LineFollowerAction, LineFollowerFeedback, LineFollowerResult, LineFollowerGoal
 
 def line_follower_client():
-    # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
-    client = actionlib.SimpleActionClient('line_follower_client', LineFollowerAction)
+
+    client = actionlib.SimpleActionClient('line_follower', LineFollowerAction)
 
     # Waits until the action server has started up and started
     # listening for goals.
@@ -19,9 +17,8 @@ def line_follower_client():
 
     # Creates a goal to send to the action server.
     goal = LineFollowerGoal()
-    goal.goal.position.x = 2
-    goal.goal.position.y = -4
-    goal.goal.position.z = 0
+    goal.goal.time = 2
+    goal.goal.distance = 3
 
     # Sends the goal to the action server.
     client.send_goal(goal)
@@ -29,12 +26,15 @@ def line_follower_client():
     # Waits for the server to finish performing the action.
     client.wait_for_result()
 
-    # Prints out the result of executing the action
+    # Gets the result of executing the action
     return client.get_result()
 
+
 if __name__ == '__main__':
+
     # Initializes a rospy node so that the SimpleActionClient can
-    # publish and subscribe over ROS.
+    # publish and subscribe over ROS. 
     rospy.init_node('line_follower_client')
     result = line_follower_client()
-    print(result)
+
+
--- ca_tools/scripts/line_follower_server.py
+++ ca_tools/scripts/line_follower_server.py
@@ -4,12 +4,12 @@ import roslib
 roslib.load_manifest('ca_tools')
 import rospy
 import actionlib
-import time
 
-import ca_actions.msg
-from follow_lines import FollowLines
+from ca_actions.msg import LineFollowerAction, LineFollowerFeedback, LineFollowerResult
 from ground_truth import GroundTruth
 from robot_localization_tf import RobotLocalizationTf
+from std_msgs.msg import Bool, Float64
+from geometry_msgs.msg import Pose
 
 
 class LineFollowerServer:
@@ -18,53 +18,113 @@ class LineFollowerServer:
     """
 
     # create messages that are used to publish feedback/result
-    _feedback = ca_actions.msg.LineFollowerFeedback()
-    _result = ca_actions.msg.LineFollowerResult()
+    _feedback = LineFollowerFeedback()
+    _result = LineFollowerResult()
+    _goal_diff_threshold = 0.1
 
     def __init__(self):
-        self._action_name = name
-        rospy.loginfo("1")
-        self._as = actionlib.SimpleActionServer(self._action_name, ca_actions.msg.LineFollowerAction, execute_cb=self._execute_cb, auto_start = False)
-        rospy.loginfo("2")
+        self._as = actionlib.SimpleActionServer('line_follower', LineFollowerAction, self._execute_cb, False)
+        self._move_robot_pub = rospy.Publisher('/line_follower_is_on', Bool, queue_size=10)
         self._as.start()
         self._gt = GroundTruth()
         self._time_elapsed = 0
         self._distance_travelled = 0
         self._last_pose = self._gt.get_pose()
         self._last_time = rospy.get_time()
+        self._r = rospy.Rate(1)
 
     def _execute_cb(self, goal):
         
         """ Callback function for updating current data and checking whether the goal has been reached
         """
 
-        rospy.loginfo("entered cb")
+        move_robot = Bool()
+        move_robot.data = True
+        self._move_robot_pub.publish(move_robot)
+
         # Begin assuming that the goal is not reached
         success = False
-        # Get current state of the robot
-        self._current_pose = self._gt.get_pose()
-        self._current_time = rospy.get_time()
-        # Compute time and distance variations from last callback until now
-        _, _, _, pose_diff = RobotLocalizationTf.get_pose_diff(self._current_pose,self._last_pose)
-        self._last_pose = self._current_pose
-        time_diff = rospy.get_time() - self._last_pose
-        # Update time and distance travelled
-        self._feedback.distance += pose_diff
-        self._feedback.time += time_diff
-        self._feedback.position.x = self._current_pose
-        # Check if goal has been reached
-        success = _, _, _, pose_diff = RobotLocalizationTf.get_pose_diff(self._current_pose,goal)
+
+        while(self._time_elapsed < goal.goal.time and self._distance_travelled < goal.goal.distance):
+
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                self._as.set_preempted()
+                success = False
+                move_robot.data = False
+                self._move_robot_pub.publish(move_robot)
+                break
+
+            # Get current state of the robot
+            self._current_pose = self._gt.get_pose()
+            self._current_time = rospy.get_time()
+            # Compute time and distance variations from last callback until now
+            pose_diff = RobotLocalizationTf.get_position_diff(self._current_pose.position,self._last_pose.position)
+            self._last_pose = self._current_pose
+            time_diff = rospy.get_time() - self._last_time
+            # Update time and distance travelled
+            self._feedback.feedback.distance += pose_diff
+            self._feedback.feedback.time += time_diff
+            self._feedback.feedback.position = self._last_pose.position
+            self._as.publish_feedback(self._feedback)
+
+            # Check if goal has been reached
+            pose_diff = RobotLocalizationTf.get_position_diff(self._current_pose.position,goal.goal.position)
+            success = pose_diff < self._goal_diff_threshold #hardcode
+
 
         if success:
             self._result.result = self._feedback.feedback
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
+            move_robot.data = False
+            self._move_robot_pub.publish(move_robot)
 
-    def run(self, goal):
-        self.server.set_succeeded()
+        self._r.sleep()
 
 
 if __name__ == '__main__':
     rospy.init_node('line_follower_server')
-    server = LineFollowerServer(rospy.get_name())
+    server = LineFollowerServer()
     rospy.spin()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""while ((self.distance_to_goal() > self.distance_tolerance)
+            and (self._feedback.time_elapsed < self._duration_threshold)
+            and (self._times_oop < self._times_oop_threshold)):
+        # check that preempt has not been requested by the client
+        if self._as.is_preempt_requested():
+            self._as.set_preempted()
+            success = False
+            break
+
+        self._feedback.time_elapsed = rospy.Time.now()-self._starting_time
+        self._feedback.distance_moved = self._accumulated_distance
+        # publish the feedback
+        self._as.publish_feedback(self._feedback)
+        r.sleep() # repeate every 1 sec
+
+    if success:
+        self._stop_moving()
+        self._move_flag = False
+        self._result.total_time = rospy.Time.now() - self._starting_time
+        rospy.loginfo('%s: Succeeded', self._action_name)
+        self._result.result = True
+        self._as.set_succeeded(self._result)
+"""
