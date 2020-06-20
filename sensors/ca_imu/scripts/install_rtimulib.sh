3a7fc6ba47383abbfa2bcf98d021cc2c78cae55d
Emiliano Borghi <eborghiorue@frba.utn.edu.ar>
2020-06-20 16:29:52 -0300

    Fix OGL in Travis

--- sensors/ca_imu/scripts/install_rtimulib.sh
+++ sensors/ca_imu/scripts/install_rtimulib.sh
@@ -9,13 +9,13 @@ RTIMULib_INSTALL="${RTIMULib_PATH}/Linux/install"
 mkdir -p ${RTIMULib_PATH}
 git clone https://github.com/RoboticaUtnFrba/RTIMULib.git ${RTIMULib_PATH}
 cd ${RTIMULib_PATH}
+# Disable compilation with OpenGL in Travis
+# https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+BUILD_GL=""
+[ "${CI}" == "true" ] && BUILD_GL="-DBUILD_GL=OFF"
 # Build and install RTIMULib
 mkdir -p ${RTIMULib_BUILD}
 cd ${RTIMULib_BUILD}
-# Disable OpenGL in Travis
-# https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
-BUILD_GL=""
-[ CI == "true" ] && BUILD_GL="-DBUILD_GL=OFF"
 cmake ${BUILD_GL} -DCMAKE_INSTALL_PREFIX=${RTIMULib_INSTALL} ..
 make -j4
 make install
